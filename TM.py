9a552a99c201252f6165fa11e5c4fb87e14bc411
ppguz <ppguzmanmayen@gmail.com>
2024-11-19 21:22:54 -0600

    Feat: added a cipher function, refacted step function

--- TM.py
+++ TM.py
@@ -15,7 +15,7 @@ class TuringMachine:
         self.current_state = initial_state
 
     def load_tape(self, input_string):
-        self.tape = list(input_string.upper()) + ['_']  # Convertir a mayúsculas y agregar símbolo en blanco
+        self.tape = list(input_string.upper()) + [" "]  # Convertir a mayúsculas y agregar símbolo en blanco
         self.head_position = 0
         print(f"Cinta cargada: {''.join(self.tape)}")  # Depuración
 
@@ -24,25 +24,24 @@ class TuringMachine:
 
     def step(self):
         current_symbol = self.tape[self.head_position]
-        transition_key = f"({self.current_state}, {current_symbol})"
-        print(f"Buscando clave: {transition_key}")
-        transition = self.transitions.get(transition_key)
-        if not transition:
-            print(f"No hay transición válida para la clave {transition_key}.")
-            return False  # No valid transition, halt
-        next_state, write_symbol, direction = transition
-        print(f"Transición: ({self.current_state}, {current_symbol}) -> ({next_state}, {write_symbol}, {direction})")
-        self.tape[self.head_position] = write_symbol
-        self.current_state = next_state
-        self.head_position += 1 if direction == 'R' else -1
-        return True
+        print(f"Current symbol: {current_symbol}")
+        tape2_symbol = self.output_tape[self.head_position]
+        print(f"Current symbol tape2: {tape2_symbol}")
+        print(f"Memory: {self.memory}")
+        values = self.transitions[self.current_state][self.memory][f"({current_symbol},{tape2_symbol})"]
+        self.current_state = values["next_state"]
+        print(f"Next state: {self.current_state}")
+        self.memory = values["memory_value"]
+        self.output_tape[self.head_position] = values["tape2_value"]
+        if values["movement"] == "R":
+            if self.head_position != len(self.tape) - 1:
+                self.head_position += 1
     
     def cipher(self, input_message):
         self.load_tape(input_message)
         self.generate_output_tape()
         while self.current_state not in self.accept_states:
-
-            pass
+            self.step()
         pass
 
     def run(self):
@@ -61,4 +60,12 @@ print(machine.input_alphabet)
 
 print(machine.output_tape)
 
-print(machine.transitions["q0"][" "]["(A, )"])
+print(machine.transitions["q0"][" "]["(D, )"])
+
+
+
+
+
+machine.cipher("D#HOLA")
+
+print(machine.output_tape)
